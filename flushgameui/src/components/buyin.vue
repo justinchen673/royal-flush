a4b048817166e078d152d17fca61c619348dd850
zdward <zacharydward12@gmail.com>
2021-04-18 23:48:42 -0400

    retrying to add this folder..

---
+++ flushgameui/src/components/buyin.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="bar">
+    <ol>
+        <li class ="Buyheader">Buy In</li>
+        <li class="amount">(Pay {{ baseAmount }})</li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'buyin',
+  props: {
+	baseAmount: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.bar {
+	background: #34eb86;
+	border: 3px solid green;
+	border-radius: 50px;
+  width: 27%;
+  margin-top: 100px;
+  margin-left: 0px;
+}
+
+.bar:hover {
+  background: #138719;
+  transition: ease-out 0.5s;
+}
+
+.Buyheader {
+  font-family: 'Poppins', sans-serif;
+  font-size: 25px;
+  margin-right: 50px;
+}
+
+</style>
---
+++ flushgameui/src/components/topbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="topbar">
+    <ol>
+        <li class="title">{{ title }}</li>
+    </ol>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+  props: {
+	title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.topbar {
+	position: absolute;
+	background-color: #348ceb;
+	top: 0px;
+	border: 3px solid black;
+	border-radius: 10px;
+	width: 100%;
+}
+
+.topbar:hover {
+  background-color: blue;
+  transition: ease-out 0.5s;
+}
+
+
+.title {
+	color: white;
+	font-family: 'Poppins', sans-serif;
+  font-size: 30px;
+}
+</style>
